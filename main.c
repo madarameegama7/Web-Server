47adb0b3e224326e50e2dd41a4cf6104c61154e6
Madara Meegama <madarameegama7@gmail.com>
2025-02-15 18:47:18 +0530

    Modified main.c to handle multithreading

--- main.c
+++ main.c
@@ -1,64 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <winsock2.h> //windows specific header fornworking with network sockets
-#include <windows.h> //windows specific header for working with windows API
+#include <winsock2.h>
+#include <windows.h>
 #include "./client/handleclient.h"
-#include "server_config.h"
 
-#pragma comment(lib, "ws2_32.lib")  //tells the compiler to link the Winsock library (ws2_32.lib), which is necessary for the socket-related functions to work correctly on Windows.
-
-void init_winsock() {
-    WSADATA wsa;
-    if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
-        printf("Winsock initialization failed. Error Code : %d\n", WSAGetLastError());
-        exit(EXIT_FAILURE);
-    }
+DWORD WINAPI handle_client_thread(LPVOID client_socket) {
+    SOCKET client_fd = *(SOCKET*)client_socket;
+    handle_client(client_fd);
+    closesocket(client_fd);
+    free(client_socket);
+    return 0;
 }
 
 int main() {
-    init_winsock();  // Initialize Winsock
+    WSADATA wsa;
+    SOCKET server_fd, *client_fd;
+    struct sockaddr_in server, client;
+    int client_size = sizeof(struct sockaddr_in);
 
-    SOCKET server_fd, client_fd;
-    struct sockaddr_in server_addr, client_addr;
-    int addrlen = sizeof(client_addr);
+    WSAStartup(MAKEWORD(2,2), &wsa);
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
-    // Create socket
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == INVALID_SOCKET) {
-        printf("Socket creation failed: %d\n", WSAGetLastError());
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(8080);
 
-    // Bind socket
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = INADDR_ANY;
-    server_addr.sin_port = htons(SERVER_PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == SOCKET_ERROR) {
-        printf("Bind failed: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-
-    // Listen for connections
-    if (listen(server_fd, MAX_CONNECTIONS) == SOCKET_ERROR) {
-        printf("Listen failed: %d\n", WSAGetLastError());
-        closesocket(server_fd);
-        WSACleanup();
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server running on port %d...\n", SERVER_PORT);
+    bind(server_fd, (struct sockaddr*)&server, sizeof(server));
+    listen(server_fd, 5);
+    
+    printf("Server running on port 8080...\n");
 
     while (1) {
-        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addrlen);
-        if (client_fd == INVALID_SOCKET) {
-            printf("Client accept failed: %d\n", WSAGetLastError());
+        client_fd = malloc(sizeof(SOCKET));
+        *client_fd = accept(server_fd, (struct sockaddr*)&client, &client_size);
+        if (*client_fd == INVALID_SOCKET) {
+            free(client_fd);
             continue;
         }
-        handle_client(client_fd);
+
+        HANDLE thread = CreateThread(NULL, 0, handle_client_thread, client_fd, 0, NULL);
+        CloseHandle(thread); // Free thread resource
     }
 
     closesocket(server_fd);
